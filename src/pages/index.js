1f4b13923d04306b444e51cbac8de58e86a984f1
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-05-13 16:57:32 +0100

    remove about and demo section for now; fix more responsiveness issues with buttons
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
       description="When we go live a useful description will go into this tag">
       <main>
         <Welcome />
-        <About />
+        {/* <About /> */}
         <Install />
-        <Demo />
+        {/* <Demo /> */}
       </main>
     </Layout>
   );
