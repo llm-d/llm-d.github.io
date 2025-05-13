968c15a451618ec3451ff810f0b223da99ca4a4d
KPRoche <kproche@us.ibm.com>
2025-05-13 16:12:34 -0700

    formatting socials better

--- docs/community/work_with_us.md
+++ docs/community/work_with_us.md
@@ -6,11 +6,11 @@ sidebar_position: 1
 
 There are several ways you can join the community effort to develop and enhance llm-d
 
-    - Via the [Github pages for llm-d: https://github.com/llm-d](https://github.com/llm-d)
-    - Via our [Slack Workspace: https://llm-d.slack.com](https://llm-d.slack.com)
+- Via the [**Github pages for llm-d:** https://github.com/llm-d](https://github.com/llm-d)**
+- Via our [**Slack Workspace:** https://llm-d.slack.com](https://llm-d.slack.com)
     
 You can also find us on
     
-    - [LinkedIn: https://linkedin.com/company/llm-d ](https://linkedin.com/company/llm-d)
-    - [X: https://x.com/\_llm_d\_](https://x.com/_llm_d_)
+- [**LinkedIn:** https://linkedin.com/company/llm-d ](https://linkedin.com/company/llm-d)
+- [**X:** https://x.com/\_llm_d\_](https://x.com/_llm_d_)
     
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -17,27 +17,7 @@ export default function Welcome() {
             <em>have</em>
           </b>
         </h2>
-        <div className="hidden-for-mobile">
-          <p>
-            While some distributed AI solutions rely on a single vendor's
-            hardware, If you have a heterogeneous compute environment that
-            includes AMD or Intel accelerators in addition to NVIDIA GPUs, you
-            want a distributed inference solution based on vLLM.
-          </p>
-
-          <ul className="ul-bulleted">
-            <li>
-              <strong>llm-d</strong> is a toolbox of components which together
-              enable distributed inference in just such a heterogenous compute
-              environment.
-            </li>
-            <li>
-              <strong>llm-d</strong> allows you to achieve distributed AI
-              inference with the compute resources you <em>have</em> rather than
-              the resources someone wishes to <em>sell</em>
-            </li>
-          </ul>
-        </div>
+        
 
         <div className="button-group">
           <button className="static-button">
@@ -61,6 +41,29 @@ export default function Welcome() {
             </a>
           </button>
         </div>
+
+        <div className="hidden-for-mobile">
+          <p>
+            While some distributed AI solutions rely on a single vendor's
+            hardware, If you have a heterogeneous compute environment that
+            includes AMD or Intel accelerators in addition to NVIDIA GPUs, you
+            want a distributed inference solution based on vLLM.
+          </p>
+
+          <ul className="ul-bulleted">
+            <li>
+              <strong>llm-d</strong> is a toolbox of components which together
+              enable distributed inference in just such a heterogenous compute
+              environment.
+            </li>
+            <li>
+              <strong>llm-d</strong> allows you to achieve distributed AI
+              inference with the compute resources you <em>have</em> rather than
+              the resources someone wishes to <em>sell</em>
+            </li>
+          </ul>
+        </div>
+        
       </div>
     </div>
   );
